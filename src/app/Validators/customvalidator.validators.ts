08c50c2216956cbe33cca4f21001d998d9f50834
Harshit Choudhary <harshit1840077@gmail.com>
2023-11-09 23:34:10 +0530

    adding Custom Validation

---
+++ src/app/Validators/customvalidator.validators.ts
@@ -0,0 +1,22 @@
+import { AbstractControl, ValidationErrors } from '@angular/forms';
+
+export class CustomValidator {
+  static checkDob(control: AbstractControl): ValidationErrors | null {
+   
+    const dob = new Date(control.value);
+    const currentDate = new Date();
+    
+    if (dob > currentDate) {
+      return { checkDob: true }; 
+    }
+
+    return null; 
+  }
+
+  static noNumbers(control: AbstractControl): ValidationErrors | null {
+    if (isNaN(Number(control.value))) {
+      return { noNumbers: true };
+    }
+    return null;
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { CustomValidator } from './Validators/customvalidator.validators';
+
 
 @Component({
   selector: 'app-root',
@@ -9,28 +11,29 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class AppComponent {
   title = 'Reactive-forms';
 
-  // Create form with controls
+
   form = new FormGroup({
     firstname: new FormControl('', [Validators.required]),
     lastname: new FormControl('', [Validators.required]),
     middlename: new FormControl('', [Validators.required]),
-    dob: new FormControl('', [Validators.required]),
+    dob: new FormControl('', [Validators.required, CustomValidator.checkDob]),
     studentId: new FormControl('', [Validators.required]),
     address: new FormControl('', [Validators.required]),
     country: new FormControl('', [Validators.required]),
-    city: new FormControl('', [Validators.required]),
+    city: new FormControl('', [Validators.required,CustomValidator.noNumbers]),
     branch: new FormControl('', [Validators.required]),
     course: new FormControl('', [Validators.required]),
     terms: new FormControl('', [Validators.required]),
-
+    gender: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
   });
-
-
-  onSubmit(f: any) {
-    if (!f.valid) {
-      f.markAllAsTouched();
+  onSubmit(form:any) {
+    if (form.invalid) {
+     form.markAllAsTouched();
+      
     }
-  }
-  
 
+    
+    
+  }
 }
